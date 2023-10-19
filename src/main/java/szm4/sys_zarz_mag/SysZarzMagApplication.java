03c44767d9305977b0071ceac48ec2727ea88ea3
Jediss <kubaczobot@gmail.com>
2023-10-19 12:49:44 +0200

    First Commit

---
+++ src/main/java/szm4/sys_zarz_mag/SysZarzMagApplication.java
@@ -0,0 +1,13 @@
+package szm4.sys_zarz_mag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysZarzMagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysZarzMagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/szm4/sys_zarz_mag/SysZarzMagApplicationTests.java
@@ -0,0 +1,13 @@
+package szm4.sys_zarz_mag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysZarzMagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
